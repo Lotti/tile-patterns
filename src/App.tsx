ae75a10104e375c9107c5abedce9e1c9dd64fcb0
Lotti <lotti86@gmail.com>
2023-07-29 17:21:19 +0200

    migrated to vite

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import {Component} from 'react';
+import './App.css';
+
+import Tile from './Tile';
+
+class App extends Component {
+    render() {
+        const tilePerRow = 15;
+
+        const tiles = [];
+        for (let i = 0; i < tilePerRow; i++) {
+            // note: we add a key prop here to allow react to uniquely identify each
+            // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+            tiles.push(<Tile key={i} />);
+        }
+
+
+        return (
+            <div className="container">
+                <div className="wall">
+                    <div className="row odd">
+                        {tiles}
+                    </div>
+                    <div className="row">
+                        {tiles}
+                    </div>
+                    <div className="row odd">
+                        {tiles}
+                    </div>
+                    <div className="row">
+                        {tiles}
+                    </div>
+                    <div className="row odd">
+                        {tiles}
+                    </div>
+                    <div className="row">
+                        {tiles}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Tile.tsx
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+
+class Tile extends Component {
+    state = {className: 'w'};
+
+    toggle = () => {
+        if (this.state.className === 'w') {
+            this.setState({className: 'y'});
+        } else {
+            this.setState({className: 'w'});
+        }
+    };
+
+    render() {
+        return (
+            <span className={'tile '+this.state.className} onClick={this.toggle} />
+        );
+    }
+}
+
+export default Tile;
